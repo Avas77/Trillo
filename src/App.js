518cb942238b6ccda11c3dccb2190cf3808afc88
Avas Bajracharya <insanebuzz09@gmail.com>
2020-10-18 17:28:15 +0545

    Done till description part

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.scss';
 import logo from './assets/logo.png';
 import sprite from './assets/sprite.svg';
 import user from './assets/user.jpg';
+import hotel1 from './assets/hotel-1.jpg';
+import hotel2 from './assets/hotel-2.jpg';
+import hotel3 from './assets/hotel-3.jpg';
+import user1 from './assets/user-3.jpg';
+import user2 from './assets/user-4.jpg';
+import user3 from './assets/user-5.jpg';
+import user4 from './assets/user-6.jpg';
 
 function App() {
   return (
@@ -10,7 +17,7 @@ function App() {
       <header className="header">
         <img src={logo} alt="" className="logo"/>
         <form action="#" className="search">
-          <input type="text" className="search__input" placeholder = "Search hotels...."/>
+          <input type="text" className="search__input" placeholder = "Search Hotels...."/>
           <button className="search__button">
             <svg className="search__icon">
               <use xlinkHref = {sprite + "#icon-magnifying-glass"}></use>
@@ -77,7 +84,81 @@ function App() {
           </div>
         </nav>
         <main className="description">
-          Description part
+          <div className="gallery">
+            <figure className="gallery__items">
+              <img src={hotel1} alt="" className="gallery__photo"/>
+            </figure>
+            <figure className="gallery__items">
+              <img src={hotel2} alt="" className="gallery__photo"/>
+            </figure>
+            <figure className="gallery__items">
+              <img src={hotel3} alt="" className="gallery__photo"/>
+            </figure>
+          </div>
+          <div className="overview">
+            <h1 className="overview__heading">Hotel Annapurna</h1>
+            <div className="overview__stars">
+              <svg className="overview__icon">
+                <use xlinkHref = {sprite + "#icon-star"} />
+              </svg>
+              <svg className="overview__icon">
+                <use xlinkHref = {sprite + "#icon-star"} />
+              </svg>
+              <svg className="overview__icon">
+                <use xlinkHref = {sprite + "#icon-star"} />
+              </svg>
+              <svg className="overview__icon">
+                <use xlinkHref = {sprite + "#icon-star"} />
+              </svg>
+              <svg className="overview__icon">
+                <use xlinkHref = {sprite + "#icon-star"} />
+              </svg>
+            </div>
+            <div className="overview__location">
+              <svg className="overview__icon-location">
+                <use xlinkHref = {sprite + "#icon-location-pin"} />
+              </svg>
+              <button className="btn-inline">Budanikantha, Kathmandu</button>
+            </div>
+            <div className="overview__rating">
+              <div className="overview__rating-average">8.6</div>
+              <div className="overview__rating-score">426 votes</div>
+            </div>
+          </div>
+          <div className="detail">
+            <div className="more">
+              <p className="paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi nisi dignissimos debitis ratione sapiente saepe. Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate.
+              </p>
+              <p className="paragraph">
+              Accusantium cumque, quas, ut corporis incidunt deserunt quae architecto voluptate delectus, inventore iure aliquid aliquam.
+              </p>
+              <ul className="list">
+                <li className="list__item">Close to the mountain</li>
+                <li className="list__item">Breakfast included</li>
+                <li className="list__item">Free airport shuffle</li>
+                <li className="list__item">Free wifi in all rooms</li>
+                <li className="list__item">Air conditioning and heating</li>
+                <li className="list__item">Pets allowed</li>
+                <li className="list__item">We speak all languages</li>
+                <li className="list__item">Perfect for families</li>
+              </ul>
+              <div className="recommend">
+                <p className="recommend__count">
+                  Saruna and 3 other friends recommend this hotel.
+                </p>
+                <div className="recommend__friends">
+                  <img src={user1} alt="" className="recommend__photo"/>
+                  <img src={user2} alt="" className="recommend__photo"/>
+                  <img src={user3} alt="" className="recommend__photo"/>
+                  <img src={user4} alt="" className="recommend__photo"/>
+                </div>
+              </div>
+            </div>
+            <div className="user-reviews">
+              User Review part
+            </div>
+          </div>
         </main>
       </div>
     </div>
